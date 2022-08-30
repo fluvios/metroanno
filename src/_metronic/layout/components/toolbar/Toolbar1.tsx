5d092e29ec673519f4696bf92f61b8a3dc32ccdd
fachriannoor <fachriannoor@bukittechnology.com>
2022-08-30 19:15:16 +0700

    merge with template

---
+++ src/_metronic/layout/components/toolbar/Toolbar1.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import clsx from 'clsx'
+import {useState} from 'react'
+import {KTSVG} from '../../../helpers'
+import {CreateAppModal} from '../../../partials'
+import {useLayout} from '../../core'
+import {DefaultTitle} from '../header/page-title/DefaultTitle'
+
+const Toolbar1 = () => {
+  const {classes} = useLayout()
+  const [showCreateAppModal, setShowCreateAppModal] = useState<boolean>(false)
+
+  return (
+    <>
+      <div className='toolbar' id='kt_toolbar'>
+        {/* begin::Container */}
+        <div
+          id='kt_toolbar_container'
+          className={clsx(classes.toolbarContainer.join(' '), 'd-flex flex-stack')}
+        >
+          <DefaultTitle />
+
+          {/* begin::Actions */}
+          <div className='d-flex align-items-center py-1'>
+            {/* begin::Wrapper */}
+            <div className='me-4'>
+              {/* begin::Menu */}
+              {/* end::Menu */}
+            </div>
+            {/* end::Wrapper */}
+
+            {/* begin::Button */}
+            {/* end::Button */}
+          </div>
+          {/* end::Actions */}
+        </div>
+        {/* end::Container */}
+      </div>
+      <CreateAppModal show={showCreateAppModal} handleClose={() => setShowCreateAppModal(false)} />
+    </>
+  )
+}
+
+export {Toolbar1}
