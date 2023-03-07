62729e38fa7a2e72683af9510dc095874ebc9261
fachriannoor <fachriannoor@bukittechnology.com>
2023-03-07 18:05:16 +0700

    add formik

--- src/app/pages/material/material-list/annotation-modal/AnnotationEditModalForm.tsx
+++ src/app/pages/material/material-list/annotation-modal/AnnotationEditModalForm.tsx
@@ -75,96 +75,99 @@ const AnnotationEditModalForm: FC<Props> = ({feedback, material, isUserLoading})
 
   return (
     <>
-      <div className="mb-12">
-        <label className="form-label">Instruction</label>
-        <input
-          type="text"
-          className="form-control form-control-white"
-          placeholder=""
-        />
-      </div>
-      <div className="col-md-12">
-        <div className='row'>
-          <div className='col-md-6'>
-            <div className='mb-6'>
-              <label className="form-label">Subject</label>
-              <input
-                type="text"
-                className="form-control form-control-white"
-                placeholder=""
-                disabled
-              />
-            </div>
-            <div className='mb-6'>
-              <label className="form-label">Learning Outcomes</label>
-              <input
-                type="text"
-                className="form-control form-control-white"
-                placeholder=""
-              />
-            </div>
-            <div className='mb-6'>
-              <label className="form-label">Materi</label>
-              <div id="materialContent" className="form-control form-control-white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Suspendisse varius tortor quis nulla cursus vulputate. 
-              Vivamus sit amet dui sollicitudin tortor placerat tincidunt. 
-              Aenean rhoncus vel orci in dignissim. 
-              Sed rutrum aliquam felis eget cursus. 
-              Mauris interdum nibh in mi blandit, sit amet porta velit scelerisque. 
-              Vestibulum turpis nunc, interdum imperdiet commodo quis, luctus vel eros. 
-              Ut at convallis dolor. Vestibulum elementum leo lacus, accumsan eleifend risus bibendum non. 
-              Ut imperdiet rhoncus commodo. Etiam facilisis ligula a dictum eleifend. 
-              Morbi molestie risus quis enim finibus, et vestibulum libero pellentesque. 
-              Sed auctor urna vel nunc efficitur sagittis. Proin porttitor luctus neque sit amet molestie. 
-              Donec diam ligula, tristique lacinia ex quis, vulputate lacinia lorem. 
-              Nam suscipit metus eget tristique tincidunt.
+      <form id='kt_modal_add_feedback_form' className='form' onSubmit={formik.handleSubmit} noValidate>
+        <div className="mb-12">
+          <label className="form-label">Instruction</label>
+          <input
+            type="text"
+            className="form-control form-control-white"
+            placeholder=""
+          />
+        </div>
+        <div className="col-md-12">
+          <div className='row'>
+            <div className='col-md-6'>
+              <div className='mb-6'>
+                <label className="form-label">Subject</label>
+                <input
+                  type="text"
+                  className="form-control form-control-white"
+                  placeholder=""
+                  disabled
+                />
               </div>
-            </div>            
-          </div>
-          <div className='col-md-6'>
-            <div className='mb-6'>
-              {inputList}
-              <div className="mb-6">
-                <button
-                  type='button'
-                  onClick={() => addQuestion()}
-                  className='btn btn-secondary me-3 pull-right'
-                  data-kt-users-modal-action='cancel'
-                  disabled={formik.isSubmitting || isUserLoading}>
-                  Tambah Pertanyaan
-                </button> 
-                <button
-                    type='button'
-                    //onClick={(data) => addQuestionaire(data)}
-                    className='btn btn-success me-3 pull-right'
-                    data-kt-users-modal-action='cancel'
-                    // disabled={formik.isSubmitting || isUserLoading}
-                  >
-                    Submit Pertanyaan
-                </button>                                                                     
-              </div>            
-              <div className="mb-6">
-                <label className="form-label">Feedback Catatan</label>
+              <div className='mb-6'>
+                <label className="form-label">Learning Outcomes</label>
                 <input
                   type="text"
                   className="form-control form-control-white"
                   placeholder=""
                 />
               </div>
-              <button
-                type='submit'
-                onClick={() => cancel()}
-                className='btn btn-light me-3 pull-right'
-                data-kt-users-modal-action='cancel'
-                disabled={formik.isSubmitting || isUserLoading}
-              >
-                Submit Feedback
-              </button>              
-            </div>            
+              <div className='mb-6'>
+                <label className="form-label">Materi</label>
+                <div id="materialContent" className="form-control form-control-white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Suspendisse varius tortor quis nulla cursus vulputate. 
+                Vivamus sit amet dui sollicitudin tortor placerat tincidunt. 
+                Aenean rhoncus vel orci in dignissim. 
+                Sed rutrum aliquam felis eget cursus. 
+                Mauris interdum nibh in mi blandit, sit amet porta velit scelerisque. 
+                Vestibulum turpis nunc, interdum imperdiet commodo quis, luctus vel eros. 
+                Ut at convallis dolor. Vestibulum elementum leo lacus, accumsan eleifend risus bibendum non. 
+                Ut imperdiet rhoncus commodo. Etiam facilisis ligula a dictum eleifend. 
+                Morbi molestie risus quis enim finibus, et vestibulum libero pellentesque. 
+                Sed auctor urna vel nunc efficitur sagittis. Proin porttitor luctus neque sit amet molestie. 
+                Donec diam ligula, tristique lacinia ex quis, vulputate lacinia lorem. 
+                Nam suscipit metus eget tristique tincidunt.
+                </div>
+              </div>            
+            </div>
+            <div className='col-md-6'>
+              <div className='mb-6'>
+                <form id='kt_modal_add_annotation_form' className='form' onSubmit={formik.handleSubmit} noValidate>
+                  {inputList}
+                  <div className="mb-6">
+                    <button
+                      type='button'
+                      onClick={() => addQuestion()}
+                      className='btn btn-secondary me-3 pull-right'
+                      data-kt-users-modal-action='cancel'
+                      disabled={formik.isSubmitting || isUserLoading}>
+                      Tambah Pertanyaan
+                    </button> 
+                    <button
+                        type='submit'
+                        //onClick={(data) => addQuestionaire(data)}
+                        className='btn btn-success me-3 pull-right'
+                        data-kt-users-modal-action='cancel'
+                        disabled={formik.isSubmitting || isUserLoading}
+                      >
+                        Submit Pertanyaan
+                    </button>                                                                     
+                  </div>   
+                </form>
+                <div className="mb-6">
+                  <label className="form-label">Feedback Catatan</label>
+                  <input
+                    type="text"
+                    className="form-control form-control-white"
+                    placeholder=""
+                  />
+                </div>
+                <button
+                  type='submit'
+                  onClick={() => cancel()}
+                  className='btn btn-light me-3 pull-right'
+                  data-kt-users-modal-action='cancel'
+                  disabled={formik.isSubmitting || isUserLoading}
+                >
+                  Submit Feedback
+                </button>              
+              </div>            
+            </div>
           </div>
-        </div>
-      </div>       
+        </div>       
         {/* begin::Actions */}
         <div className='text-center pt-15'>
           <button
@@ -192,7 +195,8 @@ const AnnotationEditModalForm: FC<Props> = ({feedback, material, isUserLoading})
             )}
           </button>
         </div>
-        {/* end::Actions */}      
+        {/* end::Actions */}        
+      </form>      
     </>
   )
 }
